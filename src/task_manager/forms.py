749570775281d05950835b2457ec922b348a2501
denis <den.listapad@mail.ru>
2026-03-30 23:02:36 +0300

    add form template

---
+++ src/task_manager/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.forms import  Textarea
+from django.core.validators import MinValueValidator,MaxValueValidator
+from django.core.exceptions import ValidationError
+
+from task_manager.models import Tasks
+
+
+def validate_max_count_split(value):
+    if len(value.split()) > 4:
+        raise ValidationError("%(value)s is too long. It must be less than 4 parts",
+            params={"value": value},
+        )
+
+# class TaskForm(forms.Form):
+#     name = forms.CharField(
+#         label="наименование задачи",
+#         max_length=100,
+#         validators = [
+#             validate_max_count_split,
+#         ]
+#     )
+#     priority = forms.IntegerField(
+#         label="приоритет",
+#         validators=[
+#             MinValueValidator(1),
+#             MaxValueValidator(5),
+#         ]
+#     )
+#     description = forms.CharField(
+#         required=False,
+#         label="Описание",
+#         widget=forms.Textarea(
+#             attrs={
+#                 "wid": "30",
+#                 "class": "special"
+#             }
+#         )
+#     )
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Tasks
+        fields = ["name","priority","description"]
+        widgets = {
+            "description": Textarea(attrs={"cols": 50, "rows": 5}),
+        }
---
+++ src/task_manager/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from task_manager.models import Tasks
+from account.models import User
+
+
+@receiver(post_save, sender=Tasks)
+def my_test_signal(sender,instance,created, **kwargs):
+    user = User.objects.get(id=1)
+    instance.assignee = user
+    instance.save()
